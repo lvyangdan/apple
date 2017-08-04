5805e555456026bfc69231af4cc6fdf12f77ab4d
lvyangdan <2737920271@qq.com>
2017-08-04 09:33:00 +0800

    market

--- src/ui/pages/Market/Market.js
+++ src/ui/pages/Market/Market.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import TitleHeader from '../../shared/TitleHeader/TitleHeader';
 import {connect} from 'react-redux';
-import store from '../../../redux/store'
+import img1 from './marketimg/apple1.jpg';
+import img2 from './marketimg/apple2.jpg';
+import img3 from './marketimg/apple3.jpg';
+import './Market.css'
 class Market extends React.Component{
 	render(){
-		console.log(this.props.count)
 		return(<div className='market'>
-			<TitleHeader/>
-			<p>苹果换钱：</p>
+			<TitleHeader title='market'/>
+			<img src={img1} alt=""/>
+			<img src={img2} alt=""/>
+			<img src={img3} alt=""/>
+			<p>苹果换钱：{(this.props.count.countNumber/10)+1}元</p>
 		</div>)
 	}
 }
-const mapStateToProps=(state)=>{
+const mapStateToProps= (state) => ({
 	count:state.count
-}
+})
 export default connect(mapStateToProps)(Market)
