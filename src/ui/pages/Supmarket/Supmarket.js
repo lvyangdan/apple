7b2433ecd23d51a1886482fcc7f47c032a9216d4
lvyangdan <2737920271@qq.com>
2017-08-08 21:09:12 +0800

    apple go supmarket

--- src/ui/pages/Supmarket/Supmarket.js
+++ src/ui/pages/Supmarket/Supmarket.js
@@ -2,12 +2,17 @@ import React from 'react'
 import TitleHeader from '../../shared/TitleHeader/TitleHeader'
 import {connect} from 'react-redux'
 import './Supmarket.css'
+import goods1 from './1.jpg'
+import goods2 from './2.jpg'
 class Supmarket extends React.Component{
 	render(){
 		return(<div>
 			<TitleHeader title='Supmarket'/>
 			<p>你的财富值：{this.props.market.money}</p>
-			<div className='goods'></div>
+			<div className='goods'>
+				<img src={goods1} alt=""/>
+				<img src={goods2} alt=""/>
+			</div>
 		</div>)
 	}
 }
