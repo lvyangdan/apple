b13e30f33f03ecefa623e7fa33b74305c7b661bf
lvyangdan <2737920271@qq.com>
2017-08-18 21:03:05 +0800

    pay

--- src/ui/pages/Pay/Pay.js
+++ src/ui/pages/Pay/Pay.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import TitleHeader from '../../shared/TitleHeader/TitleHeader'
 import {connect} from 'react-redux'
+import store from '../../../redux/store';
 class Pay extends React.Component{
+	pay=(e)=>{
+		e.preventDefault()
+		store.dispatch({type:'PAY'})
+	}
 	render(){
-		console.log('pay......',this.props.payment)
 		return(<div className='pay'>
 			<TitleHeader title='please choose payment way'/>
 			{this.props.payment.map(item => <li key='Math.random()'>{item}</li>)}
+			<button onClick={this.pay}></button>
 		</div>)
 	}
 }
